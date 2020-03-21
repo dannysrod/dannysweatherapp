94f6a31e5afda56b75c1c718d75e1c7040577f9b
dannysrod <dannyrod@gmail.com>
2020-03-21 11:03:18 +1100

    First Commit
    Upload of Files

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# dannysweatherapp
+# WeatherApp
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.0.
+
+## SETUP
+Run `npm i` and then get an API key and change your cities with directions below.
+
+## API Key
+Get your API key from OpenWeather (https://openweathermap.org/api) and put it on line 11 of app.component.ts
+
+## Changing Cities
+On the main app component change any of the 4 API calls city query.
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+## Build
+
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+
+## Running unit tests
+
+Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+## Running end-to-end tests
+
+Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
---
+++ src/app/app.component.ts
@@ -0,0 +1,92 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent implements OnInit {
+
+  api = '99cefb99c948d2c1072dd792abe1ae92';
+  units = 'metric';
+  city1name = 'Sydney';
+  city2name = 'Melbourne, Au';
+  city3name = 'New York';
+  city4name = 'Johannesburg';
+  city1;
+  city2;
+  city3;
+  city4;
+
+  modal = false;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.setCities();
+    this.getWeather();
+  }
+
+  setCities() {
+    if (window.localStorage) {
+      if (window.localStorage.getItem('city1name')) {
+        this.city1name = window.localStorage.getItem('city1name');
+      } else {
+        window.localStorage.setItem('city1name', this.city1name);
+      }
+      if (window.localStorage.getItem('city2name')) {
+        this.city2name = window.localStorage.getItem('city2name');
+      } else {
+        window.localStorage.setItem('city2name', this.city2name);
+      }
+      if (window.localStorage.getItem('city3name')) {
+        this.city3name = window.localStorage.getItem('city3name');
+      } else {
+        window.localStorage.setItem('city3name', this.city3name);
+      }
+      if (window.localStorage.getItem('city4name')) {
+        this.city4name = window.localStorage.getItem('city4name');
+      } else {
+        window.localStorage.setItem('city4name', this.city4name);
+      }
+    }
+  }
+
+  getWeather() {
+    const ajax1 = new XMLHttpRequest();
+    ajax1.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=' + this.city1name + '&units=' + this.units + '&appid=' + this.api, false);
+    ajax1.send();
+    this.city1 = JSON.parse(ajax1.responseText);
+
+    const ajax2 = new XMLHttpRequest();
+    ajax2.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=' + this.city2name + '&units=' + this.units + '&appid=' + this.api, false);
+    ajax2.send();
+    this.city2 = JSON.parse(ajax2.responseText);
+
+    const ajax3 = new XMLHttpRequest();
+    ajax3.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=' + this.city3name + '&units=' + this.units + '&appid=' + this.api, false);
+    ajax3.send();
+    this.city3 = JSON.parse(ajax3.responseText);
+
+    const ajax4 = new XMLHttpRequest();
+    ajax4.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=' + this.city4name + '&units=' + this.units + '&appid=' + this.api, false);
+    ajax4.send();
+    this.city4 = JSON.parse(ajax4.responseText);
+  }
+
+  changeCities(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    this.modal = true;
+  }
+
+  closeModal() {
+    this.modal = false;
+    window.localStorage.setItem('city1name', this.city1name);
+    window.localStorage.setItem('city2name', this.city2name);
+    window.localStorage.setItem('city3name', this.city3name);
+    window.localStorage.setItem('city4name', this.city4name);
+    this.getWeather();
+  }
+}
